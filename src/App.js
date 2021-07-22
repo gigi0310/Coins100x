952a3afa2a4d7ec27eb488a2bc18a4b922c978bf
Karen cai <gigi.cai0310@gmail.com>
2021-07-22 22:25:16 +1000

    Fix useEffect state supply issue

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function App() {
     };
      
     useEffect(() => {
-        
         axios
             .get(
                 "https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false"
@@ -60,10 +59,7 @@ function App() {
 
             })
             .catch((error) => console.error(error));
-    },{
-        coins:[],
-        favouriteCoins: state.favouriteCoins
-    });
+    },[]);
 
     return (
         <Router>
